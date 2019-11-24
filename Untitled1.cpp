084b34e0a699eb50d9b6c4542286a91d1d79f7e7
Vevean <57104115+Vevean@users.noreply.github.com>
2019-11-24 17:51:12 +0800

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int grade;
+	scanf("%d",&grade);
+	grade/=10;
+	switch(grade){
+	case 10:
+	case 9:
+		printf("%d",A);
+		break;
+	case 8:
+		printf("%d",B);
+		break;
+	case 7:
+		printf("%d",C);
+		break;
+	case 6:
+		printf("%d",D);
+		break;
+	case 5:
+		printf("%d",E);
+		break;
+	default :
+		printf("%d",F);
+		break;
+	return 0;
+	}
+	 
+}

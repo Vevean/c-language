21966f5cf3116557f670781b3927279fa7167e72
Vevean <57104115+Vevean@users.noreply.github.com>
2019-11-28 22:26:32 +0800

    Add files via upload

---
+++ 打印数字图形.cpp
@@ -0,0 +1,20 @@
+//打印图形(较难)
+//1
+//22
+//333
+//4444
+//55555
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<i+1;j++)
+		{
+			printf("%d",i+1);
+		}
+		printf("\n"); 
+	}
+}
